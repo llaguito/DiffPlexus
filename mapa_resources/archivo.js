30baee12142b95f3dcb722320a084a66a2923c00
Santiago Losada Borrajo <santiago.losadaborrajo@plexus.local>
2018-05-16 13:02:48 +0200

    Update diff

--- mapa_resources/archivo.js
+++ mapa_resources/archivo.js
@@ -1,48 +1,30 @@
 //Clase Archivos
-function Archivos(ficheroNew, ficheroOriginal, codigoNew, codigoOriginal, path) {
-    this.ficheroNew = ficheroNew;
-    this.ficheroOriginal = ficheroOriginal;
-    this.codigoNew = codigoNew;
-    this.codigoOriginal = codigoOriginal;
+function Archivos(nombre, path, charset) {
+    this.nombre = nombre;
     this.path = path;
+    this.charset = charset;
 
-    this.setFicheroNew = function(cambio) {
-        this.ficheroNew = cambio;
+    this.setNombre = function(cambio) {
+        this.nombre = cambio;
     }
 
-    this.getFicheroNew = function() {
-        return this.ficheroNew;
+    this.getNombre = function() {
+        return this.nombre;
     }
 
-    this.setFicheroOriginal = function(cambio) {
-        this.ficheroOriginal = cambio;
-    }
-
-    this.getFicheroOriginal = function() {
-        return this.ficheroOriginal;
-    }
-
-    this.setCodigoNew = function(cambio) {
-        this.codigoNew = cambio;
-    }
-
-    this.getCodigoNew = function() {
-        return this.codigoNew;
-    }
-
-    this.setCodigoOriginal = function(cambio) {
-        this.codigoOriginal = cambio;
-    }
-
-    this.getCodigoOriginal = function() {
-        return this.codigoOriginal;
-    }
-
-    this.setPathl = function(cambio) {
+    this.setPath = function(cambio) {
         this.Path = cambio;
     }
 
     this.getPath = function() {
         return this.Path;
+    }
+
+    this.setCharset = function(cambio) {
+        this.charset = cambio;
+    }
+
+    this.getCharset = function() {
+        return this.charset;
     }
 }
